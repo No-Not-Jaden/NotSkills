6684dc5ed7653593b873ffd8c9a801b03a123d84
Not_Jaden <jpater567@gmail.com>
2023-03-09 22:28:20 -0600

    some more bug fixes

--- src/main/java/me/jadenp/notskills/SkillEffects.java
+++ src/main/java/me/jadenp/notskills/SkillEffects.java
@@ -11,9 +11,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -56,6 +60,39 @@ public class SkillEffects implements Listener {
             }
         }
     }
+
+    @EventHandler
+    public void onProjectileLaunch(ProjectileLaunchEvent event){
+        if (event.getEntity().getShooter() instanceof Player){
+            if (selectedSkills.containsKey(((Player) event.getEntity().getShooter()).getUniqueId())) {
+                SkillLong skillLong = selectedSkills.get(((Player) event.getEntity().getShooter()).getUniqueId());
+                ((Player) event.getEntity().getShooter()).getInventory().getItemInMainHand();
+                if (((Player) event.getEntity().getShooter()).getInventory().getItemInMainHand().isSimilar(trident)) {
+                    if (skillLong.getTime() + 30000 < System.currentTimeMillis()) {
+                        selectedSkills.remove(event.getEntity().getUniqueId(), skillLong);
+                        return;
+                    }
+                    if (skillLong.getSkill() == 8) {// trident tp
+                        event.getEntity().setMetadata("skill", new FixedMetadataValue(NotSkills.getInstance(), ((Player) event.getEntity().getShooter()).getUniqueId()));
+                        selectedSkills.remove(event.getEntity().getUniqueId(), skillLong);
+                    }
+                    if (skillLong.getSkill() == 9) {
+                        // riptide
+                        ((Player)event.getEntity().getShooter()).setVelocity(event.getEntity().getVelocity());
+                        event.setCancelled(true);
+                        selectedSkills.remove(event.getEntity().getUniqueId(), skillLong);
+                    }
+                }
+            }
+        }
+    }
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event){
+        if (event.getEntity().hasMetadata("skill")){
+            if (event.getEntity().getShooter() != null)
+                ((Player) event.getEntity().getShooter()).teleport(event.getEntity().getLocation());
+        }
+    }
     @EventHandler
     public void onShoot(EntityShootBowEvent event){
         if (event.getEntity() instanceof Player){
@@ -72,7 +109,9 @@ public class SkillEffects implements Listener {
                             selectedSkills.remove(event.getEntity().getUniqueId(), skillLong);
                         } else if (skillLong.getSkill() == 5){
                             // chicken
+                            Vector velocity = event.getProjectile().getVelocity();
                             event.setProjectile(event.getEntity().getWorld().spawn(event.getProjectile().getLocation(), Chicken.class));
+                            event.getProjectile().setVelocity(velocity);
                             selectedSkills.remove(event.getEntity().getUniqueId(), skillLong);
                         }
                     }
